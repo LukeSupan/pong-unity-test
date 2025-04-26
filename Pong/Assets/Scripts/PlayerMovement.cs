3151408d7ce2d61466450facec52ee622111fda0
Luke <LukeSupan@outlook.com>
2025-04-26 17:45:54 -0400

    Updated player movement, works on controller.

--- Pong/Assets/Scripts/PlayerMovement.cs
+++ Pong/Assets/Scripts/PlayerMovement.cs
@@ -2,44 +2,49 @@ using System;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
+
+// This is player 1 movement currently, I will make a player 2 script that just calls this given move2 instead of move1
 public class PlayerControl : MonoBehaviour
 {
-
-    // Rigidbody2D used for movement
-    private Rigidbody2D _rb;
-    // Top speed for pong, serialize field so I can change it easily, this i set in inspector
-    [SerializeField] private float movespeed;
-    // New input system
-    public InputAction playerControls;
-    // It always up or down, we can simplify from this but this is good for learning
+    // Speed and vectors
+    private Rigidbody2D _rb; // Rigidbody2D, we apply velocity to this to move
+    [SerializeField] private float movespeed; // Movespeed can be changed in inspector
     Vector2 moveDirection = Vector2.zero;
 
+    // Input
+    public PlayerInputActions playerControls; // New input system, this is a script that we can use to do lots of controls, in this example I only use move, but any player input can be handled this way
+    private InputAction move; // Input action for movement, more can be made
 
+    // Initializes PlayerInputActions script
+    private void Awake()
+    {
+        playerControls = new PlayerInputActions();
+        move = playerControls.Player.Move1;
+    }
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         _rb = GetComponent<Rigidbody2D>();
-
     }
 
     // Activates player controls, required
     private void OnEnable()
     {
-        playerControls.Enable();
+        move.Enable();
     }
 
     // Deactivates player controls, required
     private void OnDisable()
     {
-        playerControls.Disable();
+        move.Disable();
     }
 
     // Fixed update to make movement smooth
-    private void FixedUpdate()
+    private void Update()
     {
         // Get the moveDirection from input system
-        moveDirection = playerControls.ReadValue<Vector2>();
+        moveDirection = move.ReadValue<Vector2>();
 
         // Apply the y movement (which is all we get), multiply by movespeed
         _rb.linearVelocityY = moveDirection.y * movespeed;
